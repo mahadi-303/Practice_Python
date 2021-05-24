8ed5e93dfe109d142a4e33d46a9655bb45aa873e
mahadi-303 <mahadihasan14753@gmail.com>
2021-05-24 10:19:34 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# working with list
+from lol.hell import arthematic
+
+
+def main():
+    hey()
+    print(arthematic(10, 20))
+
+
+def hey():
+    hello()
+
+
+def hello():
+    print("lol")
+
+
+if __name__ == '__main__':
+    main()
